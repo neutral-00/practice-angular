e3b53db2221c43978e04f738067c34472bef92eb
neutral00 <to.arvmail@gmail.com>
2025-12-14 16:42:44 +0530

    cleaned up main branch to act as the base branch(bare-bones)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Card } from './component/card/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Card],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('practice-angular');
